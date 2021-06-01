4c9f64ebcd01499ca9166dd1231608e3ce735c96
Dot-stack <olinwilliam7@gmail.com>
2021-06-01 10:36:16 +0200

    aa

--- App.js
+++ App.js
@@ -1,28 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [number, setNumber] = useState(1)
+
+  const onClickHandler = () => {
+    setNumber(2)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to</Text>
+    <View style={styles.body}>
+      <View style={styles.view1}>
       <Text style={styles.title}>WorldScape</Text>
+      </View>
+      <View style={styles.view2}>
+      <Text style={styles.text}>Hello</Text>
+      </View>
+      <View style={styles.view3}>
+      <Text style={styles.text}>3</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
     backgroundColor: '#EAE2B7',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  view1: {
+    flex: 1,
+    backgroundColor: '#EAE2B7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    flex: 1,
+    backgroundColor: '#EAE2B7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    flex: 1,
+    backgroundColor: '#D62828',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
     color: '#D62828',
     fontSize: 42,
+    textTransform: 'uppercase',
+  },
+  text: {
+    color: '#003049',
+    fontSize: 30,
+    margin: 10,
+  },
+  socialButton: {
+    width: 100,
+    height: 100,
   }
 });
