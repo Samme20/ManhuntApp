9dc9ebec1fbff86fe7586f60471ed613903e63cd
Dot-stack <olinwilliam7@gmail.com>
2021-06-01 13:26:07 +0200

    213

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button, Linking } from 'react-native';
+import { Text, View, Button, Linking, TextInput } from 'react-native';
 import styles from "./Styles";
 import React, { useState } from 'react';
 
 
 export default function App() {
+
+
+
   return (
     <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.title}>WorldScape</Text>
-      </View>
       <StatusBar style="auto" />
+ 
+      <View style={styles.content1}>
+        <Text style={styles.title}>WorldScape</Text>
+        <Text style={styles.text}></Text>
+      </View>
+
+      <View style={styles.huntedButtons}>
+        <Button title={'Hunted'}></Button>
+        <Button title={'Hunter'}></Button>
+      </View>
+      
     </View>
   );
 }
-
-
-
--- Styles.js
+++ Styles.js
@@ -1,4 +1,5 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
+
 
 //Colors
 var lemon = "#EAE2B7";
@@ -6,41 +7,41 @@ var red = "#D62828";
 var blue = "#003049";
 
 export default StyleSheet.create({
-  body: {
-    flex: 1,
-    backgroundColor: lemon,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: lemon,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    flex: 1,
-    backgroundColor: lemon,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 1,
-    backgroundColor: red,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: red,
-    fontSize: 42,
-  },
-  text: {
-    color: blue,
-    fontSize: 30,
-    margin: 10,
-  },
-  socialButton: {
-    width: 100,
-    height: 100,
-  }
+    body: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        backgroundColor: lemon,
+    },
+    title: {
+        color: red,
+        fontSize: 42,
+    },
+    content1: {
+        flex: 1.5,
+        justifyContent: 'center',
+    },
+    huntedButtons: {
+        flex: 2,
+        height: 10,
+        width: 150,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    text: {
+        color: blue,
+        fontSize: 30,
+        margin: 10,
+    },
+    socialButton: {
+        width: 100,
+        height: 100,
+    },
+    input: {
+        margin: 10,
+        borderWidth: 2,
+        borderRadius: 5,
+    },
 });
