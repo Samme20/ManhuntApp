add43850f4fccbe6ae2a051df9489236456314f2
Samuel Sundh <19sasu@stockholmscience.se>
2021-07-02 01:54:29 +0200

    redesigned login, added bug

--- src/screens/LoginTest.js
+++ src/screens/LoginTest.js
@@ -4,17 +4,14 @@ import {
   View,
   StatusBar,
   ImageBackground,
-  TouchableOpacity,
   Text,
-  TextInput
+  TextInput,
+  TouchableOpacity,
+  Dimensions
 } from "react-native";
-import { Center } from "@builderx/utils";
 import EvilIconsIcon from "react-native-vector-icons/EvilIcons";
-import { useNavigation } from "@react-navigation/native";
-
 
 function LoginTest(props) {
-  const navigation = useNavigation();
   return (
     <View style={styles.root}>
       <StatusBar barStyle="light-content" backgroundColor="rgba(0,0,0,0)" />
@@ -23,54 +20,79 @@ function LoginTest(props) {
           style={styles.rect}
           imageStyle={styles.rect_imageStyle}
           source={require("../assets/images/Gradient_JZV2kSY.png")}
-        ></ImageBackground>
-      </View>
-      <View style={styles.form}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate("Main")}
         >
-          <Center>
-            <Text style={styles.text2}>Start Hunting!</Text>
-          </Center>
-        </TouchableOpacity>
-        <View style={styles.password}>
-          <Center vertical>
-            <EvilIconsIcon name="lock" style={styles.icon2}></EvilIconsIcon>
-          </Center>
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="rgba(255,255,255,1)"
-            secureTextEntry={false}
-            style={styles.passwordInput}
-          ></TextInput>
-        </View>
-        <View style={styles.username}>
-          <Center vertical>
-            <EvilIconsIcon name="user" style={styles.icon22}></EvilIconsIcon>
-          </Center>
-          <TextInput
-            placeholder="Username"
-            placeholderTextColor="rgba(255,255,255,1)"
-            secureTextEntry={false}
-            style={styles.usernameInput}
-          ></TextInput>
-        </View>
+          <View style={styles.logoColumn}>
+            <View style={styles.logo}>
+              <View style={styles.endWrapperFiller}></View>
+              <View style={styles.text3Column}>
+                <Text style={styles.text3}>WorldScape</Text>
+                <View style={styles.rect7}></View>
+              </View>
+            </View>
+            <View style={styles.form}>
+              <View style={styles.usernameColumn}>
+
+                <View style={styles.username}>
+                  <EvilIconsIcon
+                    name="user"
+                    style={styles.icon22}
+                  ></EvilIconsIcon>
+                  <TextInput
+                    placeholder="Hunta Name!"
+                    placeholderTextColor="rgba(255,255,255,1)"
+                    secureTextEntry={false}
+                    style={styles.usernameInput}
+                  ></TextInput>
+                </View>
+
+                <View style={styles.email}>
+                  <EvilIconsIcon
+                    name="envelope"
+                    style={styles.icon22}
+                  ></EvilIconsIcon>
+                  <TextInput
+                    placeholder="Email"
+                    placeholderTextColor="rgba(255,255,255,1)"
+                    secureTextEntry={false}
+                    style={styles.emailInput}
+                  ></TextInput>
+                </View>
+
+                <View style={styles.password}>
+                  <EvilIconsIcon
+                    name="lock"
+                    style={styles.icon2}
+                  ></EvilIconsIcon>
+                  <TextInput
+                    placeholder="Password"
+                    placeholderTextColor="rgba(255,255,255,1)"
+                    secureTextEntry={false}
+                    style={styles.passwordInput}
+                  ></TextInput>
+                </View>
+              </View>
+              <View style={styles.usernameColumnFiller}></View>
+              <TouchableOpacity
+                onPress={() => props.navigation.navigate("Main")}
+                style={styles.button}
+              >
+                <Text style={styles.text2}>Start Huntin!</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.logoColumnFiller}></View>
+          <View style={styles.footerTexts}>
+            <TouchableOpacity
+              style={styles.button2}
+            >
+              <View style={styles.createAccountFiller}></View>
+              <Text style={styles.createAccount}>Create Account</Text>
+            </TouchableOpacity>
+            <View style={styles.button2Filler}></View>
+            <Text style={styles.needHelp}>Need Help?</Text>
+          </View>
+        </ImageBackground>
       </View>
-      <View style={styles.footerTexts}>
-        <TouchableOpacity
-          style={styles.button2}
-        >
-          <Text style={styles.createAccount}>Create Account</Text>
-        </TouchableOpacity>
-        <Text style={styles.needHelp}>About</Text>
-      </View>
-      <Center horizontal>
-        <View style={styles.logo}>
-          <Text style={styles.text3}>WorldScape</Text>
-          <View style={styles.rect7}></View>
-        </View>
-      </Center>
     </View>
   );
 }
@@ -81,134 +103,145 @@ const styles = StyleSheet.create({
     backgroundColor: "rgb(255,255,255)"
   },
   background: {
-    top: 0,
-    left: 0,
-    position: "absolute",
-    right: 0,
-    bottom: 0
+    flex: 1
   },
   rect: {
-    top: 0,
-    left: 0,
-    position: "absolute",
-    right: 0,
-    bottom: 0
+    flex: 1
   },
-  rect_imageStyle: {
-    opacity: 0.69
+  rect_imageStyle: {},
+  logo: {
+    width: Dimensions.get('window').width - (Dimensions.get('window').width / 4),
+    height: 111,
+    alignSelf: "center"
+  },
+  endWrapperFiller: {
+    flex: 1
+  },
+  text3: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 45,
+    marginBottom: 4,
+    textAlign: "center"
+  },
+  rect7: {
+    height: 8,
+    backgroundColor: "#25cdec",
+    marginRight: 4
+  },
+  text3Column: {
+    marginBottom: 6,
+    marginLeft: 2,
+    marginRight: -1
   },
   form: {
-    top: "40.54%",
-    left: 41,
     height: 230,
-    position: "absolute",
-    right: 41
+    marginTop: 38
+  },
+  username: {
+    height: 59,
+    backgroundColor: "rgba(251,247,247,0.25)",
+    borderRadius: 5,
+    flexDirection: "row"
+  },
+  email: {
+    height: 59,
+    backgroundColor: "rgba(251,247,247,0.25)",
+    borderRadius: 5,
+    flexDirection: "row",
+    marginTop: 27
+  },
+  icon22: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 30,
+    marginLeft: 20,
+    alignSelf: "center"
+  },
+  usernameInput: {
+    height: 30,
+    color: "rgba(255,255,255,1)",
+    flex: 1,
+    marginRight: 11,
+    marginLeft: 11,
+    marginTop: 14
+  },
+  emailInput: {
+    height: 30,
+    color: "rgba(255,255,255,1)",
+    flex: 1,
+    marginRight: 11,
+    marginLeft: 11,
+    marginTop: 14
+  },
+  password: {
+    height: 59,
+    backgroundColor: "rgba(253,251,251,0.25)",
+    borderRadius: 5,
+    flexDirection: "row",
+    marginTop: 27,
+    marginBottom: 67
+  },
+  icon2: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 33,
+    marginLeft: 20,
+    alignSelf: "center"
+  },
+  passwordInput: {
+    height: 30,
+    color: "rgba(255,255,255,1)",
+    flex: 1,
+    marginRight: 17,
+    marginLeft: 8,
+    marginTop: 14
+  },
+  usernameColumn: {},
+  usernameColumnFiller: {
+    flex: 1
   },
   button: {
-    left: 0,
     height: 59,
     backgroundColor: "rgba(31,178,204,1)",
-    position: "absolute",
-    right: 0,
-    bottom: 0,
-    borderRadius: 5
+    borderRadius: 5,
+    justifyContent: "center"
   },
   text2: {
     color: "rgba(255,255,255,1)",
-    position: "absolute"
+    alignSelf: "center"
   },
-  password: {
-    top: "37.18%",
-    left: 0,
-    height: 59,
-    backgroundColor: "rgba(253,251,251,0.25)",
-    position: "absolute",
-    right: 0,
-    borderRadius: 5
+  logoColumn: {
+    marginTop: 80,
+    marginLeft: 41,
+    marginRight: 41
   },
-  icon2: {
-    left: 20,
-    position: "absolute",
-    color: "rgba(255,255,255,1)",
-    fontSize: 33
-  },
-  passwordInput: {
-    top: "23.75%",
-    left: 61,
-    height: 30,
-    color: "rgba(255,255,255,1)",
-    position: "absolute",
-    right: 17
-  },
-  username: {
-    top: 0,
-    left: 0,
-    height: 59,
-    backgroundColor: "rgba(251,247,247,0.25)",
-    position: "absolute",
-    right: 0,
-    borderRadius: 5
-  },
-  icon22: {
-    left: 20,
-    position: "absolute",
-    color: "rgba(255,255,255,1)",
-    fontSize: 30
-  },
-  usernameInput: {
-    top: "23.63%",
-    left: 61,
-    height: 30,
-    color: "rgba(255,255,255,1)",
-    position: "absolute",
-    right: 11
+  logoColumnFiller: {
+    flex: 1
   },
   footerTexts: {
-    left: 37,
     height: 14,
-    position: "absolute",
-    right: 37,
-    bottom: 36
+    flexDirection: "row",
+    marginBottom: 36,
+    marginLeft: 37,
+    marginRight: 36
   },
   button2: {
-    left: 0,
     width: 104,
     height: 14,
-    position: "absolute",
-    bottom: 0
+    alignSelf: "flex-end"
+  },
+  createAccountFiller: {
+    flex: 1
   },
   createAccount: {
-    left: 0,
-    color: "rgba(255,255,255,0.5)",
-    position: "absolute",
-    bottom: 0
+    color: "rgba(255,255,255,0.5)"
+  },
+  button2Filler: {
+    flex: 1,
+    flexDirection: "row"
   },
   needHelp: {
     color: "rgba(255,255,255,0.5)",
-    position: "absolute",
-    right: -1,
-    bottom: 0
-  },
-  logo: {
-    top: 130,
-    width: 302,
-    height: 111,
-    position: "absolute"
-  },
-  text3: {
-    left: 2,
-    color: "rgba(255,255,255,1)",
-    position: "absolute",
-    bottom: 18,
-    fontSize: 56,
-  },
-  rect7: {
-    left: 2,
-    height: 8,
-    backgroundColor: "#25cdec",
-    position: "absolute",
-    right: 3,
-    bottom: 6
+    alignSelf: "flex-end",
+    marginRight: -1
   }
 });
 
